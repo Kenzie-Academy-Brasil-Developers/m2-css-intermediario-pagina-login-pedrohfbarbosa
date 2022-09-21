02c74aae7cef3c20c5282c25602bb43f1d412a11
Pedro Barbosa <phf.barbosa@hotmail.com>
2022-09-21 01:41:33 -0300

    inicial

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,17 @@
-/* Desenvolva seu código aqui... */
+const allButtons = document.querySelectorAll("button")
+for (let i = 0; i < allButtons.length; i++){
+    allButtons[i].addEventListener("click", function(event){
+        event.preventDefault()
+    })
+}
+
+const modalButtons = document.querySelectorAll("[data-modal-control]")
+
+for (let i = 0; i < modalButtons.length; i++){
+    let button = modalButtons[i]
+    button.addEventListener("click", function(){        
+        let modalId = button.getAttribute("data-modal-control")        
+        document.getElementById(modalId).classList.toggle("show-modal")
+    })
+}
+
